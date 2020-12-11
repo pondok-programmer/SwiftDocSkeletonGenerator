629d78b21899d9398d078cdb1ba6767ee96765a0
muizidn <muiz.idn@gmail.com>
2020-12-11 21:50:06 +0700

    Initial Commit

---
+++ SwiftDocSkeletonGenerator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftDocSkeletonGenerator
+//
+//  Created by Muis on 11/12/20.
+//  Copyright © 2020 Muis. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
